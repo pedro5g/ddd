65cf68ffecc11758c947a8e0dc0bb06e1827f6bb
Pedr0Martins13 <pedluiz321@gmail.com>
2024-10-13 13:05:47 -0300

    refactor:  add a static method to create a new slug instance

--- src/core/domain/value-objects/slug.ts
+++ src/core/domain/value-objects/slug.ts
@@ -1,20 +1,24 @@
 export class Slug {
-  public value: string
-  constructor(value: string) {
-    this.value = value
+  public value: string;
+  private constructor(value: string) {
+    this.value = value;
+  }
+
+  static create(slug: string) {
+    return new Slug(slug);
   }
 
   static toSlug(text: string): Slug {
     const __text = text
-      .normalize('NFKD')
+      .normalize("NFKD")
       .toLowerCase()
       .trim()
-      .replace(/\s+/g, '-')
-      .replace(/[^\w-]+/g, '')
-      .replace(/_/g, '-')
-      .replace(/--+/g, '-')
-      .replace(/-$/g, '')
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/_/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/-$/g, "");
 
-    return new Slug(__text)
+    return new Slug(__text);
   }
 }
